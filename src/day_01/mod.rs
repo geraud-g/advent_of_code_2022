2d5bd1beb581180a337a051ac242976f17a371b5
ster <geraud.garrigoux@gmail.com>
2022-12-01 08:24:51 +0100

    [day_01] part A + B

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,30 +1,50 @@
-use std::collections::HashSet;
-use std::iter::repeat_with;
+use std::cmp::Reverse;
 
-use advent_of_code::utils::inputs::get_file;
+use itertools::Itertools;
 
+use advent_of_code::parse_input;
+use advent_of_code::utils::inputs::{get_file, LINE_ENDING};
 
 pub fn day_01() {
-    // let inputs = get_input();
-    //
-    // let solution_a = part_a(&inputs);
-    // println!("\t- Solution A is : {}", solution_a);
-    //
-    // let solution_b = part_b(&inputs);
-    // println!("\t- Solution B is : {}", solution_b);
+    let inputs = get_input();
+
+    let solution_a = part_a(&inputs);
+    println!("\t- Solution A is : {}", solution_a);
+
+    let solution_b = part_b(&inputs);
+    println!("\t- Solution B is : {}", solution_b);
 }
 
 
-fn get_input() -> Vec<i32> {
-    unimplemented!()
+fn get_input() -> Vec<u32> {
+    let file = get_file("./src/day_01/input.txt");
+    let split_separator = format!("{}{}", LINE_ENDING, LINE_ENDING);
+    let mut elves_food = vec![];
+
+    for chunk in file.split(&split_separator) {
+        elves_food.push(
+            chunk
+                .lines()
+                .map(|l| parse_input!(l, u32))
+                .sum()
+        );
+    }
+    elves_food
 }
 
 
-fn part_a(inputs: &[i32]) -> i32 {
-    unimplemented!()
+fn part_a(elves_food: &[u32]) -> &u32 {
+    elves_food
+        .iter()
+        .max()
+        .unwrap()
 }
 
 
-fn part_b(inputs: &[i32]) -> i32 {
-    unimplemented!()
+fn part_b(elves_food: &[u32]) -> u32 {
+    elves_food
+        .iter()
+        .sorted_by_key(|w| Reverse(*w))
+        .take(3)
+        .sum()
 }
