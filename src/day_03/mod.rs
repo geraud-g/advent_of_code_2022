ded4360162d22476cf25c9f419502418e61e718a
ster <geraud.garrigoux@gmail.com>
2022-12-03 16:37:48 +0100

    [day_03] part A + B

---
+++ src/day_03/mod.rs
@@ -0,0 +1,57 @@
+use std::collections::HashSet;
+use advent_of_code::utils::inputs::get_file;
+
+
+pub fn day_03() {
+    let rucksacks = get_input();
+
+    let solution_a = part_a(&rucksacks);
+    println!("\t- Solution A is : {}", solution_a);
+
+    let solution_b = part_b(&rucksacks);
+    println!("\t- Solution B is : {}", solution_b);
+}
+
+
+fn get_input() -> Vec<Vec<char>> {
+    get_file("./src/day_03/input.txt")
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect()
+}
+
+
+fn part_a(rucksacks: &[Vec<char>]) -> i32 {
+    rucksacks
+        .iter()
+        .map(|rucksack| get_rucksack_priority(rucksack))
+        .sum()
+}
+
+
+fn get_rucksack_priority(rucksack: &[char]) -> i32 {
+    let compartment_size = rucksack.len() / 2;
+    let left_part: HashSet<_> = rucksack[0..compartment_size].iter().collect();
+    let right_part: HashSet<_> = rucksack[compartment_size..].iter().collect();
+    let common = left_part.intersection(&right_part).next().unwrap();
+    get_char_priority(common)
+}
+
+
+fn get_char_priority(ch: &char) -> i32 {
+    let value = *ch as i32;
+    if value >= 97 { value - 96 } else { value - 38 }
+}
+
+
+fn part_b(rucksacks: &[Vec<char>]) -> i32 {
+    let mut item_types = vec![];
+    let hashset_rucksacks: Vec<HashSet<_>> = rucksacks.iter().map(|r| r.iter().collect()).collect();
+
+    for rucksacks_chunk in hashset_rucksacks.chunks(3) {
+        let first_intersection: HashSet<_> = rucksacks_chunk[0].intersection(&rucksacks_chunk[1]).map(|c| c.to_owned()).collect();
+        let total_intersection = first_intersection.intersection(&rucksacks_chunk[2]);
+        item_types.push(get_char_priority(total_intersection.into_iter().next().unwrap()))
+    }
+    item_types.iter().sum()
+}
