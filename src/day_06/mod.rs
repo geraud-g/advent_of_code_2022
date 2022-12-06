683c748cbec4877d157877ee13721e99bc7ea2e3
ster <geraud.garrigoux@gmail.com>
2022-12-06 13:22:31 +0100

    [day_06] part A + B

---
+++ src/day_06/mod.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+use itertools::Itertools;
+use advent_of_code::utils::inputs::get_file;
+
+
+pub fn day_06() {
+    let datastream_buffer = get_input();
+
+    let solution_a = part_a(&datastream_buffer);
+    println!("\t- Solution A is : {}", solution_a);
+
+    let solution_b = part_b(&datastream_buffer);
+    println!("\t- Solution B is : {}", solution_b);
+}
+
+
+fn get_input() -> String {
+    get_file("./src/day_06/input.txt")
+}
+
+
+fn part_a(datastream_buffer: &str) -> usize {
+    let consecutive_characters = 4;
+    get_consecutive_distinct_characters_idx(datastream_buffer, consecutive_characters) + consecutive_characters
+}
+
+
+fn part_b(datastream_buffer: &str) -> usize {
+    let consecutive_characters = 14;
+    get_consecutive_distinct_characters_idx(datastream_buffer, consecutive_characters) + consecutive_characters
+}
+
+
+fn get_consecutive_distinct_characters_idx(text: &str, char_nbr: usize) -> usize {
+    for (idx, window) in text.chars().collect_vec().windows(char_nbr).enumerate() {
+        let hashset_window: HashSet<&char> = window.iter().collect();
+        if hashset_window.len() == char_nbr {
+            return idx;
+        }
+    }
+    panic!("Could not find {} consecutive characters for text `{}`", char_nbr, text)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod day_02;
 mod day_03;
 mod day_04;
 mod day_05;
-// mod day_06;
+mod day_06;
 // mod day_07;
 // mod day_08;
 // mod day_09;
@@ -46,7 +46,7 @@ fn solve_day(day: i32) {
         3 => day_03::day_03,
         4 => day_04::day_04,
         5 => day_05::day_05,
-        // 6 => day_06::day_06,
+        6 => day_06::day_06,
         // 7 => day_07::day_07,
         // 8 => day_08::day_08,
         // 9 => day_09::day_09,
