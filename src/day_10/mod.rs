03fd4a5a5fe68409aeb8901c229e1460cf7b89ca
ster <geraud.garrigoux@gmail.com>
2022-12-10 14:13:38 +0100

    [day_10] part A + B

---
+++ src/day_10/mod.rs
@@ -0,0 +1,101 @@
+use std::collections::HashSet;
+use std::str::FromStr;
+use advent_of_code::utils::inputs::get_file;
+
+
+pub fn day_10() {
+    let op_list = get_input();
+
+    let solution_a = part_a(&op_list);
+    println!("\t- Solution A is : {}", solution_a);
+
+    print!("\t- Solution B is :");
+    part_b(&op_list);
+}
+
+
+fn get_input() -> Vec<Op> {
+    get_file("./src/day_10/input.txt")
+        .lines()
+        .map(|line| line.parse::<Op>().unwrap())
+        .collect()
+}
+
+
+fn part_a(op_list: &[Op]) -> i32 {
+    let mut register_x = 1;
+    let mut current_cycle = 0;
+    let signal_steps: HashSet<i32> = vec![20, 60, 100, 140, 180, 220].into_iter().collect();
+    let mut sum_signal_strength = 0;
+
+    for op in op_list {
+        current_cycle += 1;
+        if signal_steps.contains(&current_cycle) {
+            sum_signal_strength += current_cycle * register_x;
+        };
+        if let Op::AddX(val) = op {
+            current_cycle += 1;
+            if signal_steps.contains(&current_cycle) {
+                sum_signal_strength += current_cycle * register_x;
+            };
+            register_x += val;
+        }
+    }
+    sum_signal_strength
+}
+
+
+fn part_b(op_list: &[Op]) {
+    let mut register_x = 1;
+    let mut current_cycle = 0;
+
+    draw_pixel(register_x, current_cycle);
+    for op in op_list.iter() {
+        current_cycle += 1;
+        draw_pixel(register_x, current_cycle);
+
+        if let Op::AddX(val) = op {
+            current_cycle += 1;
+            register_x += val;
+            draw_pixel(register_x, current_cycle);
+        }
+    }
+}
+
+fn draw_pixel(sprite_position: i32, current_cycle: i32) {
+    let current_cycle = current_cycle % 40;
+    if current_cycle == 0 {
+        println!()
+    }
+    if current_cycle >= sprite_position - 1 && current_cycle <= sprite_position + 1 {
+        print!("█")
+    } else {
+        print!(" ")
+    }
+}
+
+
+#[derive(Debug)]
+enum Op {
+    AddX(i32),
+    Noop,
+}
+
+
+impl FromStr for Op {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut words = s.split_whitespace();
+        let op = words.next().unwrap();
+
+        match op {
+            "addx" => {
+                let x = words.next().unwrap().parse::<i32>().unwrap();
+                Ok(Op::AddX(x))
+            }
+            "noop" => Ok(Op::Noop),
+            _ => Err(()),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod day_06;
 mod day_07;
 mod day_08;
 mod day_09;
-// mod day_10;
+mod day_10;
 // mod day_11;
 // mod day_12;
 // mod day_13;
@@ -50,7 +50,7 @@ fn solve_day(day: i32) {
         7 => day_07::day_07,
         8 => day_08::day_08,
         9 => day_09::day_09,
-        // 10 => day_10::day_10,
+        10 => day_10::day_10,
         // 11 => day_11::day_11,
         // 12 => day_12::day_12,
         // 13 => day_13::day_13,
